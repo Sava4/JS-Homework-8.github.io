c3fe97ae7061cb657724aaae5d61ed056bcfc21e
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-20 02:08:05 +0300

    Homework 8 done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 // Опишите своими словами, как Вы понимаете, что такое обработчик событий.
 
-alert('Hello world!');
+document.getElementById('price').onfocus = function() {
+    document.getElementById('price').style.outlineColor = 'yellowgreen';
+}
+
+const errorMsg = document.createTextNode('Please enter correct price.');
+
+
+document.getElementById('price').onblur = function() {
+    if (document.getElementById('price').value < 0) {
+        document.getElementById('price').style.borderColor = 'red';
+        document.getElementById('parent').appendChild(errorMsg);
+    } else {
+        document.getElementById('current').innerHTML = `<span class="tag is-success is-medium">Текущая цена: $${this.value}<button id="but" class="delete is-medium" onclick="rem()"></button>`;
+        document.getElementById('price').style.color = 'green';
+    }
+}
+
+const rem = () => {
+    document.getElementById('current').firstChild.remove();
+    document.getElementById('price').value = '';
+}
